864d2976df32c8665f0cc04ed0624c849abbed87
Clarence Baluyot <akingprincess@gmail.com>
2024-10-30 02:51:08 +0800

    again

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -9,4 +9,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py create_superuser --no-input
-pipenv run gunicorn django_try.wsgi:application --bind 0.0.0.0:8000
