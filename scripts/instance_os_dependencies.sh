0b7cab403056fdcc0968f3048a3685ddc7e96847
Clarence Baluyot <akingprincess@gmail.com>
2024-10-30 01:11:48 +0800

    additional dependencies

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,6 +1,12 @@
 
 sudo apt install -y nginx pipenv
 
-cd /home/ubuntu/django_try
+cd /home/ubuntu/django_try/
 
 pipenv install
+pipenv shell
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+python manage.py create_superuser
+python manage.py runserver 0.0.0.0:8000
