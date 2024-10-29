cdab2d9e710dfb58e0f27508380d6f971369e287
Clarence Baluyot <akingprincess@gmail.com>
2024-10-30 01:24:18 +0800

    gunicorn

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,12 +1,12 @@
-
 sudo apt install -y nginx pipenv
 
 cd /home/ubuntu/django_try/
 
 pipenv install
+pipenv install gunicorn django
 pipenv shell
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
-python manage.py create_superuser
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+python manage.py create_superuser --no-input
+pipenv run gunicorn django_try.wsgi:application --bind 0.0.0.0:8000
